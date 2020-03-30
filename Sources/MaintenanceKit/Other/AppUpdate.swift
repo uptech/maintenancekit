a58d29d482acb1ebe1b0fdbcbe9629eebc039250
Anthony Castelli <hello@anthonyc.dev>
2020-03-30 15:29:46 -0700

    Missing Public getter

--- Sources/MaintenanceKit/Other/AppUpdate.swift
+++ Sources/MaintenanceKit/Other/AppUpdate.swift
@@ -10,16 +10,16 @@ import Foundation
 /// Returns all of the required info for an app update
 public struct AppUpdate {
     /// Determines if an update is avaialble
-    let updateAvailable: Bool
+    public let updateAvailable: Bool
     
     /// Returns true or false if the app is older than the minimum version
-    let isOlderThanMinimumVersion: Bool
+    public let isOlderThanMinimumVersion: Bool
     
     /// Is the update required
-    let isRequired: Bool
+    public let isRequired: Bool
     
     /// Platform upgrade details
-    let details: Platform
+    public let details: Platform
     
     internal init(updateAvailable: Bool, isOlderThanMinimumVersion: Bool, details: Platform) {
         self.updateAvailable = updateAvailable
